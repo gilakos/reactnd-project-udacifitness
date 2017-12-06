2cdd791af848d2e9aca3dc301cbead87f0d82883
Gil Akos <gil@astra.finance>
2017-12-06 14:06:18 -0800

    Feat: Implement redux store

--- udacifitness/App.js
+++ udacifitness/App.js
@@ -2,42 +2,21 @@ import React from 'react'
 import {
   Text,
   View,
-  StyleSheet,
-  TouchableHighlight,
-  ToubhableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Slider
 } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
 
 export default class App extends React.Component {
-  state = {
-    value: 0,
-  }
-
   render() {
     return (
-
-      <View style={styles.container}>
-        <Slider
-          value={this.state.value}
-          onValueChange={(value) => this.setState(() => ({ value }))}
-        />
-        <Text>
-          Value: {this.state.value}
-        </Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    alignItems: "stretch",
-    justifyContent: "center"
-  }
-})
